31ed0ea8ca602002c6215a119b297f7f4e791f4f
Mattmatt2211 <114118152+Mattmatt2211@users.noreply.github.com>
2025-06-05 18:16:19 -0500

    Delete header.js

--- header.js
+++
@@ -1,8 +0,0 @@
-fetch('/header.html')
-  .then(res => res.text())
-  .then(html => {
-    const container = document.getElementById('global-header');
-    if (container) {
-      container.innerHTML = html;
-    }
-  });
